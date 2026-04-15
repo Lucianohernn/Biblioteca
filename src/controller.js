ed09d451c2c66ed21becd5d76d9ea28171fff687
Lucianohernn <hermannluciano@gmail.com>
2026-04-15 14:01:21 -0300

    creada la estructura

---
+++ src/controller.js
@@ -0,0 +1,13 @@
+//consultas en la base de datos
+
+import {pool} from './database.js';
+
+class LibroController{
+
+    async getAll(req, res){
+        const [result] = await pool.query('SELECT * FROM libros');
+        res.json(result);
+    }
+}
+
+export const libro = new LibroController();

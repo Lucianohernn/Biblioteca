a67349e53f24fc190238f11a7eee6636eaf6bae5
Lucianohernn <hermannluciano@gmail.com>
2026-04-22 09:24:07 -0300

    agregado metodo add

--- src/controller.js
+++ src/controller.js
@@ -16,11 +16,18 @@ class LibroController{
         res.json(result);
     }
 
-    /*async delete(req, res){
+    async add(req, res){
+        const libro = req.body;
+        const [result] = await pool.query(`INSERT INTO libros(nombre, autor, categoria, fecha_publicacion, isbn) VALUES (?, ?, ?, ?, ?)`, [libro.nombre, libro.autor, libro.categoria, libro.fecha_publicacion, libro.isbn])
+        res.json({"Libro insertado": result.insertId})
+    }
+
+
+    async delete(req, res){
         const libro = req.body;
         const [result] = await pool.query(`DELETE FROM libros where isbn=(?)`, [libro.isbn]);
         res.json({"Libro eliminado": result.affectedRows});
-    }*/
+    }
 
     
 }
--- src/routes.js
+++ src/routes.js
@@ -5,4 +5,5 @@ export const router = Router()
 
 router.get('/libros', libro.getAll);
 router.get('/libros/:id', libro.getOne);
-//router.delete('/libro', libro.delete);
+router.post('/libro/', libro.add);
+router.delete('/libro', libro.delete);
